600241807f42ca6dd8c0add8991ba3693e2aa4f1
S. A. Wicaksono <supriyantoagungwicaksono@gmail.com>
2026-01-22 10:46:17 +0800

    creating user models

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const userStructure = new Schema(
+    {
+        username : {
+            type : String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, //menghapus automatis whitespaces(karakter kosong / spasi)
+            minLength: 5,
+            maxLength: 10,
+        },
+
+        password : {
+            type: String,
+            required: true,
+            unique: true,
+            minLength: 8,
+            maxLength: 20
+        },
+
+        email : {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+
+    },
+
+    {
+        timestamps: true
+    }
+
+)
+
+export default userStructure
