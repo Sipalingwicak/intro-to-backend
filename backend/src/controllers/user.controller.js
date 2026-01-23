d434d3ebc2434e2aef0a91515369bdea7374bf65
SUPRIYANTO AGUNG WICAKSONO <supriyantoagungwicaksono@gmail.com>
2026-01-23 10:24:24 +0800

    nambahin logout route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -72,4 +72,27 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { registerUser, loginUser };
+const logoutUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({
+      email,
+    });
+
+    if (!user)
+      return res.status(404).jso({
+        message: "User not found!",
+      });
+
+    res.status(200).json({
+      message: "Logout Sucess!",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Status Error",
+    });
+  }
+};
+
+export { registerUser, loginUser, logoutUser };
